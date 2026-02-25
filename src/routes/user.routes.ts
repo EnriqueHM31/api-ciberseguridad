9e7b62700761a59306422292eb67f91925906ac5
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-25 11:50:38 -0600

    refactor: corregir importación y uso del middleware de verificación de administrador para mayor claridad

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,12 @@ import type { Router as ExpressRouter } from 'express';
 import { Router } from 'express';
 import { UserController } from '../controller/UserController.controller';
 import { validarCrearUsuarioMiddleware, validarIdUsuarioMiddleware, validarModificarUsuarioMiddleware } from '../middleware/user.middleware';
-import { verificarAdmin } from '../middleware/verifyAuthToken.middleware';
+import { verificarAdminMiddleware } from '../middleware/verifyAuthToken.middleware';
 
 export const userRouter: ExpressRouter = Router();
 
 // USERS
-userRouter.get('/', verificarAdmin, UserController.ObtenerUsuarios);
-userRouter.post('/', verificarAdmin, validarCrearUsuarioMiddleware, UserController.CrearUsuario);
-userRouter.put('/:id_usuario', verificarAdmin, validarIdUsuarioMiddleware, validarModificarUsuarioMiddleware, UserController.ModificarUsuario);
-userRouter.delete('/:id_usuario', verificarAdmin, validarIdUsuarioMiddleware, UserController.EliminarUsuario);
+userRouter.get('/', verificarAdminMiddleware, UserController.ObtenerUsuarios);
+userRouter.post('/', verificarAdminMiddleware, validarCrearUsuarioMiddleware, UserController.CrearUsuario);
+userRouter.put('/:id_usuario', verificarAdminMiddleware, validarIdUsuarioMiddleware, validarModificarUsuarioMiddleware, UserController.ModificarUsuario);
+userRouter.delete('/:id_usuario', verificarAdminMiddleware, validarIdUsuarioMiddleware, UserController.EliminarUsuario);
