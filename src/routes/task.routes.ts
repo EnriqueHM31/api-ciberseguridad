3c13893001d46936b7297780d11c183c78a3103a
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-25 11:50:35 -0600

    refactor: corregir importación y uso del middleware de verificación de usuario para mayor claridad

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -2,12 +2,12 @@ import type { Router as ExpressRouter } from 'express';
 import { Router } from 'express';
 import { TareasController } from '../controller/TareasController.controller';
 import { validarCrearTareaMiddleware, validarIdTareaMiddleware, validarIdUsuarioTareaMiddleware } from '../middleware/task.middleware';
-import { verificarUser } from '../middleware/verifyAuthToken.middleware';
+import { verificarUserMiddleware } from '../middleware/verifyAuthToken.middleware';
 
 export const taskRouter: ExpressRouter = Router();
 
 // TASKS
-taskRouter.get('/user/:id_usuario', verificarUser, validarIdUsuarioTareaMiddleware, TareasController.ObtenerTareasUsuario);
-taskRouter.post('/', verificarUser, validarCrearTareaMiddleware, TareasController.CrearTarea);
-taskRouter.put('/:id_tarea', verificarUser, validarIdTareaMiddleware, TareasController.ModificarTarea);
-taskRouter.delete('/:id_tarea', verificarUser, validarIdTareaMiddleware, TareasController.EliminarTarea);
+taskRouter.get('/user/:id_usuario', verificarUserMiddleware, validarIdUsuarioTareaMiddleware, TareasController.ObtenerTareasUsuario);
+taskRouter.post('/', verificarUserMiddleware, validarCrearTareaMiddleware, TareasController.CrearTarea);
+taskRouter.put('/:id_tarea', verificarUserMiddleware, validarIdTareaMiddleware, TareasController.ModificarTarea);
+taskRouter.delete('/:id_tarea', verificarUserMiddleware, validarIdTareaMiddleware, TareasController.EliminarTarea);
