1b6ddbedb912fc95675b205f64bbb522ec63853f
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-25 11:50:31 -0600

    refactor: corregir nombres de middleware para mayor claridad

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -1,15 +1,21 @@
 import type { Router as ExpressRouter } from 'express';
 import { Router } from 'express';
 import { PasswordController } from '../controller/PasswordController.controller';
-import { resetLimiter, resetLimitPassword } from '../middleware/limiteIntentos.middleware';
-import { validarCambiarPassword, validarRequestReset, validarResetearPassword, validarResetPasswordFinal, validarVerifyReset } from '../middleware/password.middleware';
-import { verificarResetPassword } from '../middleware/verifyResetPassword.middleware';
+import { resetLimiterMiddleware, resetLimitPasswordMiddleware } from '../middleware/limiteIntentos.middleware';
+import {
+    validarCambiarPasswordMiddleware,
+    validarRequestResetMiddleware,
+    validarResetearPasswordMiddleware,
+    validarResetPasswordFinalMiddleware,
+    validarVerifyResetMiddleware,
+} from '../middleware/password.middleware';
+import { verificarResetPasswordMiddleware } from '../middleware/verifyResetPassword.middleware';
 
 export const passwordRouter: ExpressRouter = Router();
 
 // PASSWORD
-passwordRouter.put('/reset/:id_usuario', validarResetearPassword, PasswordController.ResetearContraseña);
-passwordRouter.put('/change/:id_usuario', resetLimitPassword, validarCambiarPassword, PasswordController.CambiarContraseña);
-passwordRouter.post('/request-reset', resetLimiter, validarRequestReset, PasswordController.requestReset);
-passwordRouter.put('/verify-reset', resetLimiter, validarVerifyReset, PasswordController.verifyReset);
-passwordRouter.put('/reset-password-login', verificarResetPassword, validarResetPasswordFinal, PasswordController.resetPassword);
+passwordRouter.put('/reset/:id_usuario', validarResetearPasswordMiddleware, PasswordController.ResetearContraseña);
+passwordRouter.put('/change/:id_usuario', resetLimitPasswordMiddleware, validarCambiarPasswordMiddleware, PasswordController.CambiarContraseña);
+passwordRouter.post('/request-reset', resetLimiterMiddleware, validarRequestResetMiddleware, PasswordController.requestReset);
+passwordRouter.put('/verify-reset', resetLimiterMiddleware, validarVerifyResetMiddleware, PasswordController.verifyReset);
+passwordRouter.put('/reset-password-login', resetLimitPasswordMiddleware, verificarResetPasswordMiddleware, validarResetPasswordFinalMiddleware, PasswordController.resetPassword);
