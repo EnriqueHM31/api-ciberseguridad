a2b8b300987c90d5a4b437602e22e4f15e417268
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-02-25 11:50:27 -0600

    refactor: corregir importación del middleware de verificación de token para mayor claridad

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,11 +2,11 @@ import type { Router as ExpressRouter } from 'express';
 import { Router } from 'express';
 import { AuthController } from '../controller/AuthController.controller';
 import { validarLoginMiddleware } from '../middleware/auth.middleware';
-import { verificarToken } from '../middleware/verifyAuthToken.middleware';
+import { verificarTokenMiddleware } from '../middleware/verifyAuthToken.middleware';
 
 export const authRouter: ExpressRouter = Router();
 
 // AUTH
 authRouter.post('/login', validarLoginMiddleware, AuthController.IniciarSesion);
-authRouter.post('/logout', verificarToken, AuthController.CerrarSesion);
+authRouter.post('/logout', verificarTokenMiddleware, AuthController.CerrarSesion);
 authRouter.post('/verify', AuthController.VerificarUsuario);
